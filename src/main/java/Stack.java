b7e818162d2f216f9feaa05bd97a5317f00861fe
niti44 <hce073bct025@hcoe.edu.np>
2022-01-17 10:14:48 +0545

    hello world

--- src/main/java/Stack.java
+++ src/main/java/Stack.java
@@ -2,6 +2,7 @@ public class Stack {
     public static void main(String[] args) {
         System.out.println("STACKS");
         System.out.println("QUEUES");
+        System.out.println("hello world");
 
     }
 }
